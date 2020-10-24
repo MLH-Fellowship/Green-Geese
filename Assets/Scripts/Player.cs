b1e79476a8482ca702a75d20c894889a7a1c9ae6
Gerald <gprmarquez@gmail.com>
2020-10-24 04:50:07 -0700

    initial possible pivot into platform game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is so there will always be the RigidBody 2D Component in the inspector
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+
+    public float movementSpeed = 10f;
+
+    // Assigning rb as Rigidbody2D component 
+    Rigidbody2D rb;
+
+    float movement = 0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // For side-to-side movement of character
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+}
