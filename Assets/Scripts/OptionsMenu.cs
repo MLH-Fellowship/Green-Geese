e8f9cc1f9e5d07f7a79e5627b08ef4b2f31b66d8
Gerald <gprmarquez@gmail.com>
2020-10-25 21:11:57 -0700

    connected volume in options to MainMixer

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// must be using Unity Engine Audio to call on the exposed parameter of the Unity Audio Mixer
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    // to get a float value for "volume" using the slider
+    public void SetVolume(float volume)
+    {
+        // sets the AudioMixer value to volume 
+        audioMixer.SetFloat("volume", volume);
+    }
+}
